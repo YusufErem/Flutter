19e617a388846899706555c5a7cb287df3bbc533
Yusuf Ömer Erem <ysferem@gmail.com>
2022-10-03 14:44:31 +0300

    Lambda

---
+++ koleksyonlaraOzgumetotlar/LambdaNedir.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  Function fonksiyon1 = (int a, int b) {
+    return a * b;
+  }; //  gibi bir fonksiyon olusutabilirim burda fonksiyonu bir degisken gibi olustaribliyorum cunki dartta fonksiyonlarda
+  //birer nesnedir
+
+  print(fonksiyon1(5, 5));
+}
